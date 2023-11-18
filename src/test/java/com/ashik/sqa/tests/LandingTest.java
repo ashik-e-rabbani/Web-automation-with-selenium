d3525b5a3b337d0ed731bfe73a4d17ab88409db8
Ashik Rabbani <ashik.ashik.rabbni@gmail.com>
2023-11-18 13:37:13 +0600

    Added Log4j for better logging
    Step 1: Add Log4j Dependency
    Step 2: Create Log4j Configuration File
    Step 3: Use Log4j in Your Classes

--- src/test/java/com/ashik/sqa/tests/LandingTest.java
+++ src/test/java/com/ashik/sqa/tests/LandingTest.java
@@ -3,6 +3,8 @@ package com.ashik.sqa.tests;
 import com.ashik.sqa.utils.BrowserFactory;
 import com.ashik.sqa.utils.DriverManager;
 import com.ashik.sqa.utils.enums;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,6 +15,7 @@ import static com.ashik.sqa.utils.BrowserFactory.quitParallelDriver;
 
 public class LandingTest {
 
+    private static final Logger log = LogManager.getLogger(LandingTest.class);
     private WebDriver driver_predefined, driver_sequential_from_factory, driver_parallel_from_factory;
 
 
@@ -26,6 +29,8 @@ public class LandingTest {
 
         // Get parallel driver from factory
         driver_parallel_from_factory = BrowserFactory.createParallelDriver(enums.BrowserType.CHROME);
+
+        log.info("Driver setUp complete.");
     }
 
 
@@ -33,6 +38,7 @@ public class LandingTest {
     public void PredefinedDriverTest() throws InterruptedException {
         // Your test logic using the pre-defined driver
         driver_predefined.get("https://web.tallykhata.com");
+        log.info("PredefinedDriverTest executed successfully.");
 
     }
 
@@ -40,12 +46,14 @@ public class LandingTest {
     public void SequentialDriverFromFactoryTest() throws InterruptedException {
         // Your test logic using the pre-defined driver
         driver_sequential_from_factory.get("https://tallykhata.com");
+        log.info("SequentialDriverFromFactoryTest executed successfully.");
 
     }
     @Test(priority = 3, description = "This will use parallel driver from BrowserFactory class")
     public void ParallelDriverFromFactoryTest() throws InterruptedException {
         // Your test logic using the driver from factory
         driver_parallel_from_factory.get("https://ashik-e-rabbani.github.io");
+        log.info("ParallelDriverFromFactoryTest executed successfully.");
 
     }
     @AfterClass
