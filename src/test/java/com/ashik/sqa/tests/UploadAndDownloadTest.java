aaacd61279079a3a1e2a2b2e2f6ae72bb972664a
Ashik Rabbani <ashik.ashik.rabbni@gmail.com>
2023-12-05 08:10:25 +0600

    fixed #4

--- src/test/java/com/ashik/sqa/tests/UploadAndDownloadTest.java
+++ src/test/java/com/ashik/sqa/tests/UploadAndDownloadTest.java
@@ -1,8 +1,11 @@
 package com.ashik.sqa.tests;
 
+import com.ashik.sqa.pages.LinkPage;
 import com.ashik.sqa.pages.UploadAndDownloadPage;
 import com.ashik.sqa.utils.BrowserFactory;
 import com.ashik.sqa.utils.enums;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -14,11 +17,13 @@ import java.util.Map;
 
 public class UploadAndDownloadTest {
 
+    private static final Logger log = LogManager.getLogger(UploadAndDownloadTest.class);
     WebDriver driver;
-    ChromeOptions chromeOptions; UploadAndDownloadPage uadp;
+    ChromeOptions chromeOptions;
+    UploadAndDownloadPage uadp;
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
         // Set Chrome options for download preferences
         chromeOptions = new ChromeOptions();
         chromeOptions.setExperimentalOption("prefs",
@@ -27,8 +32,9 @@ public class UploadAndDownloadTest {
                         "download.directory_upgrade", true,
                         "safebrowsing.enabled", true));
 
-        driver = BrowserFactory.createParallelDriver(enums.BrowserType.CHROME,chromeOptions);
+        driver = BrowserFactory.createParallelDriver(enums.BrowserType.CHROME, chromeOptions);
         uadp = new UploadAndDownloadPage(driver);
+        log.info("Driver initialized.");
 
     }
 
@@ -37,23 +43,24 @@ public class UploadAndDownloadTest {
         try {
             driver.get("https://demoqa.com/upload-download");
         } catch (Exception e) {
+            log.error("Error occurred when navigating to page. - " + e);
             throw new RuntimeException(e);
         }
     }
 
     @Test(priority = 1)
-    public void downloadImage(){
+    public void downloadImage() {
         Assert.assertTrue(uadp.downloadImage());
     }
-    @Test(priority = 2)
-    public void uploadFile(){
 
+    @Test(priority = 2)
+    public void uploadFile() {
         Assert.assertTrue(uadp.uploadFile("/home/ashik/Pictures/gift.jpg"));
     }
 
     @AfterClass
-    public void tearUp(){
-        if (driver !=null){
+    public void tearUp() {
+        if (driver != null) {
             BrowserFactory.quitParallelDriver();
         }
     }
